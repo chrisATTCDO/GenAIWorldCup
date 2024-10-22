5c060251609240fcc6821d84ab4f8e88fbbc78a3
tagirisapavan <pavantagirisa@outlook.com>
2024-10-22 10:04:11 -0500

    Update config.py

--- UI/config.py
+++ UI/config.py
@@ -1,3 +1,3 @@
 # config.py  
-PAT = "dapiba23fab912d57906d913aea38a4b0fa8-2"  
-URL = "https://adb-2754134964726624.4.azuredatabricks.net/serving-endpoints/stockchat/invocations" 
+PAT = "PAT TOKEN"  
+URL = "DATABRICKS SERVING ENDPOINT URL" 
